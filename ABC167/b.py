9c204b30626fa6c1f95b4e6b2b438e1e70ee577e
kanata <kanahira24@outlook.jp>
2021-02-03 09:22:18 +0900

    ABC167 b

---
+++ ABC167/b.py
@@ -0,0 +1,13 @@
+def main():
+    a, b, c, k = map(int, input().split())
+    ans = 0
+
+    if a>=k:
+        print(k)
+    elif a+b>=k:
+        print(a)
+    else:
+        print(a-(k-(a+b)))
+        
+if __name__ == '__main__':
+    main()
