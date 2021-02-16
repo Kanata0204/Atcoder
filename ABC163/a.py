4773a18b6cbb36bd6de5d2df0dcfec5ee52c4a29
kanata <kanahira24@outlook.jp>
2021-02-16 22:28:36 +0900

    ABC163 a b

---
+++ ABC163/a.py
@@ -0,0 +1,9 @@
+def main():
+    pie = 3.1415926534
+    
+    r = int(input())
+    
+    print(2 * pie * r)
+    
+if __name__ == '__main__':
+    main()
---
+++ ABC163/b.py
@@ -0,0 +1,13 @@
+def main():
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    
+    s = sum(a)
+    
+    if s > n:
+        print(-1)
+    else:
+        print(n-s)
+    
+if __name__ == '__main__':
+    main()
