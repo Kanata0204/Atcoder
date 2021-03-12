89f7f058ddcfa95110aa9c19fe15613bac30bf9c
kanata <kanahira24@outlook.jp>
2021-03-12 21:53:11 +0900

    146 c ccomplete

--- ABC146/c.py
+++ ABC146/c.py
@@ -1,53 +1,26 @@
+a, b, x = map(int, input().split())
+
+def cost(n):
+    return a * n + b * len(str(n))
+
 def main():
-    a, b, x = map(int, input().split())
-    pre_n = 1
-    pre_i = 1
+    bottom = 1
+    top = 10**9
     
-    print(a, b,x)
+    if cost(1) > x:
+        exit(print(0))
     
-    if (a >= 10**9 or b >= 10**9) and x >= 10 ** 9:
+    if cost(10**9)  <= x:
         exit(print(10**9))
-    
-    while(True):
-        s = a * pre_n + b * pre_i
-        print('s:',pre_i,  s)
         
-        if s > x:
-            # print(s, pre_i, pre_n)
-            break
-    
-        pre_i += 1
-        pre_n *= 10
-        
-    s -= a + b # pre_i -= 1
-    # print(s)
-    pre_i -= 1
-    print('s-x', s-x)
-    syo = (s - x) // a + 1
-    print(syo)
-    tmp = s - a * syo
-    print(tmp)
-    
-    # print(s, len(str(s)))
-    print(pre_i)
-    
-    # print('---------------')
-    
-    if s <= x:
-        exit(print((tmp-(b*pre_i))//a))
-    elif s - a * ((s - x) // a + 1) > 10**9:
-        print(10**9)
-    else:
-        syo = (s - x) // a + 1
-        # print(syo)
-        # print(s-x)
-        # print((s - x) // a)
-        # print((s - x) // a + 1)
-        # print(a * syo)
-        tmp = s - a * syo
-        # print(tmp)
-        print((tmp-(b*pre_i))//a)
-        
+    while(top-bottom > 1):
+        mid = (top + bottom) // 2
+        if cost(mid) <= x:
+            bottom = mid
+        else:
+            top = mid
+            
+    print(bottom)
     
 if __name__ == '__main__':
     main()
